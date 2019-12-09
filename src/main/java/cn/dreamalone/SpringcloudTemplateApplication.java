6adc4c5cdbf0dac9489f32732090ff481ce0c27b
leitu <tulei0507@yahoo.com>
2019-12-09 17:57:59 +0800

    构建项目

---
+++ src/main/java/cn/dreamalone/SpringcloudTemplateApplication.java
@@ -0,0 +1,13 @@
+package cn.dreamalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudTemplateApplication.class, args);
+    }
+
+}
